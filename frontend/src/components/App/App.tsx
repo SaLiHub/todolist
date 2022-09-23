c14142a1b2cf440048784d73ceefebb3a80d7e1d
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-09-24 00:59:32 +0300

    Add dark theme

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
-
-import './App.css';
+import './App.sass';
 
 import { Navigate, Route, Routes } from "react-router-dom";
-import SignIn from "../../auth/SignIn";
-import SignUp from "../../auth/SignUp";
-import Profile from "../Profile";
+import SignIn from "../Sign/SignIn";
+import SignUp from "../Sign/SignUp";
+import Profile from "../Profile/Profile";
+import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
 
     const isAuth = localStorage.getItem('isAuth');
 
     return (
-        <div className="App">
-            {isAuth === 'true' ?
-                <Routes>
-                    <Route path="/profile" element={<Profile/>}/>
-                    <Route
-                        path="*"
-                        element={<Navigate to="/profile" replace/>}
-                    />
-                </Routes> :
-                <Routes>
-                    <Route path="/sign-in" element={<SignIn/>}/>
-                    <Route path="/sign-up" element={<SignUp/>}/>
-                    <Route
-                        path="*"
-                        element={<Navigate to="/sign-in" replace/>}
-                    />
-                </Routes>
-            }
-        </div>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline/>
+                <div className="App">
+                    {isAuth === 'true' ?
+                        <Routes>
+                            <Route path="/profile" element={<Profile/>}/>
+                            <Route
+                                path="*"
+                                element={<Navigate to="/profile" replace/>}
+                            />
+                        </Routes> :
+                        <Routes>
+                            <Route path="/sign-in" element={<SignIn/>}/>
+                            <Route path="/sign-up" element={<SignUp/>}/>
+                            <Route
+                                path="*"
+                                element={<Navigate to="/sign-in" replace/>}
+                            />
+                        </Routes>
+                    }
+                </div>
+
+            </ThemeProvider>
+        </StyledEngineProvider>
     );
+
 }
 
 export default App;
