e70fbdecaf083184d76ac05c772fdf35bd1085b9
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-09-23 17:18:47 +0300

    Add verification of email

--- frontend/src/hooks/useSignIn.ts
+++ frontend/src/hooks/useSignIn.ts
@@ -1,19 +1,23 @@
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
-import {useRef, useState} from "react";
+import { useRef, useState } from "react";
 import { Error } from './useSignUp';
+import { isValidEmail } from "../helpers/helper.todolist";
 
 export const useSignIn = () => {
 
-    const navigate = useNavigate();
     const [error, setError] = useState<Error | null>();
     const checkboxRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+    const emailRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (!isValidEmail(emailRef.current.value)) {
+            setError({name: 'error', message: 'Email is not valid'});
+            return;
+        }
+
         const data = new FormData(event.currentTarget);
-        console.log(data)
         const dataToSend = {
             email: data.get('email'),
             password: data.get('password'),
@@ -23,17 +27,20 @@ export const useSignIn = () => {
         axios.post(`http://localhost:3001/api/v1/sign-in`, dataToSend, {withCredentials: true})
             .then((res) => {
                 const {message, done} = res.data;
-                if(done) {
-                    navigate("/profile", { replace: true });
+                if (done) {
+                    localStorage.setItem('isAuth', 'true');
+                    window.location.href = '/profile';
+                    // navigate("/profile", {replace: true});
                 } else {
                     setError({name: 'error', message});
                 }
-            }).catch(e => console.log(e))
+            }).catch(e => console.log(e));
     };
 
     return {
         handleSubmit,
-        error,
-        checkboxRef
+        emailRef,
+        checkboxRef,
+        error
     }
 }
