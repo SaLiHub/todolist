5337a7371d0f11cbf3c4af83c6fe62ae7bf109b5
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-09-23 19:33:40 +0300

    Add authentication state

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,24 +2,36 @@ import React from 'react';
 
 import './App.css';
 
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SignIn from "../../auth/SignIn";
 import SignUp from "../../auth/SignUp";
 import Profile from "../Profile";
 
 function App() {
-  return (
-      <div className="App">
-        <Routes>
-          <Route path="/sign-in" element={<SignIn/>}/>
-          <Route path="/sign-up" element={<SignUp/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route
-            path="/"
-            element={<Navigate to="/sign-in" replace />}
-          />
-        </Routes>
-      </div>
-  );
+
+    const isAuth = localStorage.getItem('isAuth');
+
+    return (
+        <div className="App">
+            {isAuth === 'true' ?
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route
+                        path="*"
+                        element={<Navigate to="/profile" replace/>}
+                    />
+                </Routes> :
+                <Routes>
+                    <Route path="/sign-in" element={<SignIn/>}/>
+                    <Route path="/sign-up" element={<SignUp/>}/>
+                    <Route
+                        path="*"
+                        element={<Navigate to="/sign-in" replace/>}
+                    />
+                </Routes>
+            }
+        </div>
+    );
 }
+
 export default App;
